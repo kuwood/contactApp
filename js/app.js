37c06399d64069f41d4bf5e51b15704d7bc3df11
Keith Underwood <kuwood@users.noreply.github.com>
2016-06-17 20:11:58 -0600

    Finish Contact features

--- js/app.js
+++ js/app.js
@@ -1,35 +1,44 @@
 var contactsArray = []
 
-function Contact(first,last,phone,street,city,state) {
-  this.first = first;
-  this.last = last;
-  this.phone = phone;
-  this.street = street;
-  this.city = city;
-  this.state = state;
+function Contact(first, last, phone, street, city, state) {
+    this.first = first;
+    this.last = last;
+    this.phone = phone;
+    this.street = street;
+    this.city = city;
+    this.state = state;
 }
 
 function createContact() {
-  var c = new Contact($('#firstName').val(),$('#lastName').val(),$('#phone').val(),$('#street').val(),$('#city').val(),$('#state').val());
-  contactsArray.push(c);
-  console.log('created!');
+    var c = new Contact($('#firstName').val(), $('#lastName').val(), $('#phone').val(), $('#street').val(), $('#city').val(), $('#state').val());
+    contactsArray.push(c);
+    console.log('created!');
 }
 
 function clearList() {
-  $('.contactList ul').children().remove();
+    $('.contactList ul').children().remove();
 }
 
 function populateList() {
-  contactsArray.forEach(function(c) {
-    $('.contactList ul').append('<li><a>' + c.first + ' ' + c.last + '</a></li>')
-  })
+    contactsArray.forEach(function(c) {
+        var item = $('<li data-id="' + contactsArray.indexOf(c) + '"><a>' + c.first + ' ' + c.last + '</a></li>')
+        item.click(function() {
+            $('#contactTitle').text(c.first + " " + c.last);
+            $('#showFirst').text("First name: " + c.first)
+            $('#showLast').text("Last name: " + c.last)
+            $('#showPhone').text("Phone number: " + c.phone)
+            $('#showAddressTitle').text("Addresses:")
+            $('#addressList').append('<li>' + c.street + " " + c.city + ", " + c.state + '</li>')
+        })
+        $('.contactList ul').append(item)
+    })
 }
 
-
 $(document).ready(function() {
-  $("#add").click(function() {
-    createContact();
-    clearList();
-    populateList();
-  })
+    $("#addContact").submit(function() {
+        createContact();
+        clearList();
+        populateList();
+        return false;
+    })
 })
